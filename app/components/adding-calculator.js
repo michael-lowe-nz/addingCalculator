c9fd8e50ac39e30569c4287b625d237d0bf594c9
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-05-21 22:46:13 +1200

    Can't figure out how to push to an array in ember

--- app/components/adding-calculator.js
+++ app/components/adding-calculator.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  currentInput: null,
   numbers: [1,5,7],
 
   sumNumbers: Ember.computed('numbers', function () {
     return this.get('numbers').reduce((current, sum) => current + sum, 0);
   }),
+
+  actions: {
+    add: function() {
+      this.set('numbers', 'numbers'.pushObject(4))
+    }
+  }
 });
